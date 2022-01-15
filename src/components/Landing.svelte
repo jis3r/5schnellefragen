215b79625ade0511322bbe90101a7f24b8706488
jis3r <jis3r@protonmail.com>
2022-01-15 20:12:39 +0100

    add: Landing.svelte-component

---
+++ src/components/Landing.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    const start = () => {
+        dispatch("start");
+    }
+
+</script>
+<div class="test twelve columns">
+    <button class="button-primary" on:click={start}>Gib mir 5!</button>
+</div>
+
+<style>
+    button { 
+        bottom: 25%;
+    }
+    .test {
+        bottom: 0;
+    }
+</style>
