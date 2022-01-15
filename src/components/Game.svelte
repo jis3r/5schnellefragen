f134d7e09cbdb1f9b00e027bb31997916558db5b
jis3r <jis3r@protonmail.com>
2022-01-15 20:12:14 +0100

    add: Game.svelte-component with question-functionality

---
+++ src/components/Game.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Question from './Question.svelte';
+    import { onMount, createEventDispatcher } from "svelte";
+    import { getQuesitons } from '../utils/data';
+
+    const dispatch = createEventDispatcher();
+
+    let questions = getQuesitons();
+    let pos = 0;
+
+
+    onMount(() => {
+        console.log(questions);
+    });
+
+    const next = () => {
+        if(pos === 4) dispatch("back")
+        pos = pos + 1;
+    }
+</script>
+
+<Question details={questions[pos]}/>
+<button on:click={next}>next</button>
+
