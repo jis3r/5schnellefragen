0ef2cd5b92a42b8c99aa8e3b94df92eb3680dded
jis3r <jis3r@protonmail.com>
2022-01-15 20:11:14 +0100

    add: Question.svelte-component

---
+++ src/components/Question.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let details = {  id: "-1", 
+                            question: "Frage",
+                            episode: "-1",
+                            year: "2000",
+                            author: "dump"
+
+    }
+</script>
+
+<h3 class="question">{details.question}</h3>
+<p class="episode">{details.episode}</p>
+<p class="year">{details.year}</p>
+<p class="author">{details.author}</p>
