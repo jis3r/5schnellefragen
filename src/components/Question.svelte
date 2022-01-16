d0985187c131fa29ed936f70362750a5a726e21e
jis3r <jis3r@protonmail.com>
2022-01-16 19:19:13 +0100

    add: new design for question-metadata

--- src/components/Question.svelte
+++ src/components/Question.svelte
@@ -8,7 +8,25 @@
     }
 </script>
 
-<h3 class="question">{details.question}</h3>
-<p class="episode">{details.episode}</p>
-<p class="year">{details.year}</p>
-<p class="author">{details.author}</p>
+<div class="row" style="margin-top: 25%">
+    <div class="twelve columns">
+        <h3 class="question">{details.question}</h3>
+    </div>
+</div>
+<div class="row">
+    <div class="twelve columns">
+        <span class="episode">Folge {details.episode}</span>
+        <span class="year">{details.year}</span>
+        <span class="author">Von {details.author}</span>
+    </div>
+</div>
+
+<style>
+span { 
+    color: #33C3F0 /*#006C73*/;
+    background-color: rgba(51, 195, 240, 0.25);
+    border-radius: 0.25rem;
+    padding: 0.125rem 0.625rem;
+    margin: 0 0.25rem;
+}
+</style>
