6b18a6abf448fb41f420e8ecb07a4cbaebc9ebeb
jis3r <jis3r@protonmail.com>
2022-01-17 21:01:33 +0100

    fix: removed unused code and changed game-button-naming

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -1,17 +1,12 @@
 <script>
     import Question from './Question.svelte';
-    import { onMount, createEventDispatcher } from "svelte";
+    import { createEventDispatcher } from "svelte";
     import { getQuesitons } from '../utils/data';
 
     const dispatch = createEventDispatcher();
 
     let questions = getQuesitons();
     let pos = 0;
-
-
-    onMount(() => {
-        console.log(questions);
-    });
 
     const next = () => {
         if(pos === 4) dispatch("back")
@@ -20,4 +15,4 @@
 </script>
 
 <Question details={questions[pos]}/>
-<button class="button-primary button-low" on:click={next}>next</button>
+<button class="button-primary button-low" on:click={next}>nächste frage</button>
