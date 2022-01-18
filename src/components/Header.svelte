307442be414b36b704172ede5290e5dcea439074
jis3r <jis3r@protonmail.com>
2022-01-18 20:07:44 +0100

    add: hilarious media-queries for header to be one-line-only

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,5 +7,48 @@
 <style>
     h1 {
         font-weight: bold;
+        font-size: 3.2rem;
+    }
+    @media (min-width: 322px) {
+        h1 {
+            font-size: 3.3rem;
+        }
+    }
+    @media (min-width: 330px) {
+        h1 {
+            font-size: 3.4rem;
+        }
+    }
+    @media (min-width: 338px) {
+        h1 {
+            font-size: 3.5rem;
+        }
+    }
+    @media (min-width: 346px) {
+        h1 {
+            font-size: 3.6rem;
+        }
+    }
+    @media (min-width: 362px) {
+        h1 {
+            font-size: 3.8rem;
+        }
+    }
+    @media (min-width: 370px) {
+        h1 {
+            font-size: 3.9rem;
+        }
+    }
+
+    @media (min-width: 380px) {
+        h1 {
+            font-size: 4rem;
+        }
+    }
+
+    @media (min-width: 550px) {
+        h1 {
+        font-size: 5rem;
+        }
     }
 </style>
