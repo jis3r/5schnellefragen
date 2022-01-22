e6028034554e98dc82fc0c87190ce525e24d10d8
jis3r <jis3r@protonmail.com>
2022-01-22 18:03:30 +0100

    add: getFilteredQuestions-function

--- src/utils/questions.js
+++ src/utils/questions.js
@@ -1,5 +1,7 @@
 import { data } from './data';
 
+let selectedQuestions;
+
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -8,8 +10,25 @@ function shuffleArray(array) {
     return array;
 }
 
-function getRandomQuestions() {
-    return shuffleArray(data).slice(0, 5);
+function getRandomQuestions(a) {
+    let array = a || data;
+    return shuffleArray(array).slice(0, 5);
+}
+
+function getFilteredQuestions(y, a) {
+    let year = y || "Alle";
+    let author = a || "Allen";
+    let filtered = data;
+
+    if(author !== "Allen") {
+        filtered = filtered.filter(q => q.author === author);
+        console.log("autorenfilter", filtered);
+    }
+    if(year !== "Alle") {
+        filtered = filtered.filter(q => q.year === year);
+        console.log("jahresfilter", filtered);
+    }
+    return getRandomQuestions(filtered);
 }
 
 function getNewestQuestions() {
@@ -20,13 +39,23 @@ function getNewestEpisode() {
     return Math.max.apply(Math, data.map(function(o) { return parseInt(o.episode); }));
 }
 
-function logQuestions(questions) {
-    let log = localStorage.getItem('questionLog') || [];
-
+function checkQuestions() {
+    let logs = localStorage.getItem('questionLog') || [];
+    if(logs.length !== 0) {
+        for (let i = 0; i < selectedQuestions.length; i++) {
+            // Find if the array contains an object by comparing the property value
+            if(logs.some(log => log.id === selectedQuestions[i].id)) {
+                alert("Object found inside the array.");
+            } else{
+                alert("Object not found.");
+            }
+        }
+    }
 }
 
 export {
     getRandomQuestions,
+    getFilteredQuestions,
     getNewestQuestions,
     getNewestEpisode
 }
