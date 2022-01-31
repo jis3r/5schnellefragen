e1f296b490787e62d72555402a457926fb1b19a7
jis3r <jis3r@protonmail.com>
2022-01-31 21:32:56 +0100

    fix: optimized bottom-padding

--- src/components/Bottom.svelte
+++ src/components/Bottom.svelte
@@ -19,14 +19,14 @@
     .links {
         padding-bottom: 15px;
     }
-    @media (min-height: 725px) {
+    @media (min-height: 735px) {
         .links {
-        position: fixed;
+        position: absolute;
         bottom: 0px;
         bottom: .5rem;
         left: 50%;
         transform: translate(-50%, 0);
-        padding-bottom: 0;
+        padding-bottom: 0px;
     }
 
     }
