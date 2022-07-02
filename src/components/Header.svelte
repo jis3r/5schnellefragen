80a77298b49efda08d8bbd51ff74143247860eb6
jis3r <jis3r@protonmail.com>
2022-07-03 00:36:49 +0200

    fix: optimized header-position

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,3 +1,3 @@
 <div class="row">
-    <img src="img/header.svg" alt="5 Schnelle Fragen" style="width: 100%; max-width: 715px; margin: auto;">
+    <img src="img/header.svg" alt="5 Schnelle Fragen" style="width: 100%; max-width: 715px; margin: 1rem auto auto auto;">
 </div>
